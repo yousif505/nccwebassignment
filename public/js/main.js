96e2e870453cfe3100bd95fca58b72fbe16f5ecd
yousif505 <ewqan505@gmail.com>
2022-02-24 17:37:14 +0300

    contact ui

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,12 @@ const closeBtn = document.querySelector('.closeBtn');
 const toggleMenu = document.querySelector('.toggleMenu');
 const menu = document.querySelector('.menu');
 
+// info HTML
+const previous_icon = document.getElementById('previous_icon');
+const next_icon = document.getElementById('next_icon');
+const infocard = document.querySelector ('.infocard');
+
+
 
 toggleMenu.addEventListener('click',() => {
     menu.classList.add('show')
@@ -9,4 +15,16 @@ toggleMenu.addEventListener('click',() => {
 
 closeBtn.addEventListener('click',() => {
     menu.classList.remove('show')
+});
+
+// To move info container towards right
+
+next_icon.addEventListener('click', () => {
+    console.log('clicked')
+    infocard.scrollLeft += 240;
+});
+
+previous_icon.addEventListener('click', () => {
+    console.log('clicked')
+    infocard.scrollLeft -= 240;
 });
